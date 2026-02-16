a9b2ca70bc8ab70f42ff13a83ea153b0756aa460
Aldebaran Lopera Ospina <aldebaran.lopera@globant.com>
2026-02-16 17:36:26 -0500

    Section implementation, component whyus

--- cdd-react-hs-project/src/main.jsx
+++ cdd-react-hs-project/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import NavBar from './components/navBar'
 import './styles/index.css'
 import Hero from './pages/layouts/Hero'
+import WhyUs from './pages/layouts/WhyUs'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <NavBar />
     <Hero/>
+    <WhyUs />
   </StrictMode>,
 )
