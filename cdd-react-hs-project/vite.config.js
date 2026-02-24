5fdffe84619bb25e07e1d2c15ef358f48e5f3085
Sauluslo <Aldebaranlopera14@gmail.com>
2026-02-24 12:52:56 -0500

    Se Configura archivo ViteConfig

--- cdd-react-hs-project/vite.config.js
+++ cdd-react-hs-project/vite.config.js
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/gym/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
+
+
+export default defineConfig(({ mode }) => {
+  // Carga las variables según el modo (dev, prod, etc.)
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/gym/api': {
+          target: env.VITE_API_URL || 'http://localhost:8080',
+          changeOrigin: true,
+        }
       }
     }
   }
